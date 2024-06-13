ecb5df9528c77ec5c56ce5c69d01a4b5146b3e33
nicolas <nicolas.programador@gmail.com>
2024-06-13 10:42:00 -0400

    crud listo, falta crud de generos

--- alumnos/urls.py
+++ alumnos/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('alumnos_del/<str:pk>', views.alumnos_del, name='alumnos_del'),
     path('alumnos_findEdit/<str:pk>', views.alumnos_findEdit, name='alumnos_findEdit'),
     path('alumnosUpdate', views.alumnosUpdate, name='alumnosUpdate'),
+
+    path('crud_generos', views.crud_generos, name="crud_generos"),
+    path('generosAdd', views.generosAdd, name="generosAdd"),
+    path('generos_del/<str:pk>', views.generos_del, name="generos_del"),
+    path('generos_edit/<str:pk>', views.generos_edit, name="generos_edit"),
 ]
--- alumnos/views.py
+++ alumnos/views.py
@@ -18,7 +18,8 @@ def alumnosAdd(request):
         generos = Genero.objects.all()
         context={"generos":generos}
         return render(request,"alumnos/alumnos_add.html",context)
-    else:        
+    else:
+        generos = Genero.objects.all()        
         #Es un post , por lo tanto se recuperan los datos del formulario
         #y se graban en la tabla
         rut = request.POST["rut"]
@@ -46,12 +47,25 @@ def alumnosAdd(request):
             activo=1
         )
         obj.save()
-        context = {"mensaje":"Ok , datos guardados....."}
+        context = {"mensaje":"Ok , datos guardados.....","generos":generos}
         return render(request,"alumnos/alumnos_add.html",context)
 
-def alumnos_del(request):
-    return "hola"
+def alumnos_del(request,pk):
+    context = {}
+    try:
+        alumno = Alumno.objects.get(rut=pk)
 
+        alumno.delete()
+
+        mensaje = "Eliminado satisfactoriamente"
+        alumnos = Alumno.objects.all()
+        context = { "alumnos": alumnos , "mensaje": mensaje }
+        return render(request,"alumnos/alumnos_list.html",context)
+    except:
+        mensaje = "Error al eliminar"
+        alumnos = Alumno.objects.all()
+        context = {"alumnos":alumnos,"mensaje":mensaje}
+        return render("alumnos/alumnos_list.html", context)
 def alumnos_findEdit(request,pk):
     if pk != "":
         alumno=Alumno.objects.get(rut=pk)
@@ -68,4 +82,41 @@ def alumnos_findEdit(request,pk):
             return render(request,"alumnos/alumnos_edit.html",context)
 
 def alumnosUpdate(request):
-    return "hola"
+    
+    if request.method == "POST":
+        print("Entra por aqui si es post")
+        rut = request.POST["rut"]
+        nombre = request.POST["nombre"]
+        aPaterno = request.POST["paterno"]
+        aMaterno = request.POST["materno"]
+        fechaNac = request.POST["fechaNac"]
+        genero = request.POST["genero"]
+        telefono = request.POST["telefono"]
+        email = request.POST["email"]
+        direccion = request.POST["direccion"]
+
+        objGenero = Genero.objects.get(id_genero = genero)
+
+        alumno = Alumno()
+        alumno.rut = rut
+        alumno.nombre = nombre
+        alumno.apellido_paterno = aPaterno
+        alumno.apellido_materno = aMaterno
+        alumno.fecha_nacimiento = fechaNac
+        alumno.id_genero = objGenero
+        alumno.telefono = telefono
+        alumno.email = email
+        alumno.direccion = direccion
+        alumno.activo = 1
+
+        alumno.save()
+
+        generos = Genero.objects.all()
+        context = {"mensaje":"Datos actualizados satisfactoriamente","generos":generos,"alumno":alumno}
+        return render(request, "alumnos/alumnos_edit.html",context)
+    else:
+        print("Si no es post")
+        #no es un post , entonces se muetra un formulario para hacer un add (insert)
+        alumnos = Alumno.objects.all()
+        context = {"alumnos":alumnos}
+        return render(request,"alumnos/alumnos_list.html",context)
